96d20915d7438dabc7baaa572a32585f8b2eec2b
Wilber Caló <the00wilber@gmail.com>
2023-04-11 09:19:27 -0300

    Projeto-finalizado

--- naoMexer.js
+++ naoMexer.js
@@ -7,7 +7,6 @@ function comprarCarta() {
   const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
   // Cria array de naipes
-  
   const naipes = ["♦️", "♥️", "♣️", "♠️"]
 
   // Sorteia uma carta
